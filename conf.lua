232acd9b55455d1ce83c7a3219b53031e558e0f5
rgrams <ross.grams@gmail.com>
2018-03-04 11:04:25 -0500

    Add conf file to attach a console

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+	t.console = true                  -- Attach a console (boolean, Windows only)
+
+	t.window.title = "Philtre2D Test Project"        -- The window title (string)
+	t.window.width = 800               -- The window width (number)
+	t.window.height = 600              -- The window height (number)
+end
