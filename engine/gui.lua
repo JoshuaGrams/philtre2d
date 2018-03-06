8bf8ea4383b3185f9df4b830e6497b68a47c0a54
rgrams <ross.grams@gmail.com>
2018-03-06 14:25:46 -0500

    GUI - update children if resized

--- engine/gui.lua
+++ engine/gui.lua
@@ -31,6 +31,12 @@ local function update(s, dt)
 	if s.lastx ~= s.lpos.x or s.lasty ~= s.lpos.y then
 		s.pos.x, s.pos.y = s.lpos.x + s.aposx, s.lpos.y + s.aposy
 	end
+	if s.children and (s.lastw ~= s.w or s.lasth ~= s.w) then
+		for i, v in ipairs(s.children) do
+			if v.parent_resized then v:parent_resized(s.w, s.h, s.origw, s.origh) end
+		end
+	end
+	s.lastw, s.lasth = s.w, s.h
 	s.lastx, s.lasty = s.lpos.x, s.lpos.y
 end
 
@@ -73,6 +79,7 @@ local function new(image, ox, oy, x, y, angle, sx, sy, ax, ay, scale_mode, kx, k
 		gui.lpos = { x=x, y=y }
 		gui.img = img
 		gui.w, gui.h = img:getDimensions()
+		gui.origw, gui.origh = gui.w, gui.h
 		gui.ox = gui.w * get_origin(ox, 0)
 		gui.oy = gui.h * get_origin(oy, 0)
 		gui.ax = get_origin(ax, 0.5)
