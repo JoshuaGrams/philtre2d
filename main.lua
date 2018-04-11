5a9baf7ed5f84354e01d8a1c51a7a11d0eb56f43
Josh Grams <josh@qualdan.com>
2018-04-11 12:31:45 -0400

    Use 'c' to toggle constraints (none/smooth/symmetric).

--- main.lua
+++ main.lua
@@ -34,6 +34,12 @@ local function endpointIndex(n)
 	return 1 + 3 * math.floor(n / 3)
 end
 
+local function otherControlPoint(curve, n)
+	local cp = 2*endpointIndex(n) - n
+	if cp < 1 or cp > #curve then cp = false end
+	return cp
+end
+
 local function extendBezier(curve, x, y)
 	local endpoint = {x, y}
 	if #curve > 0 then
@@ -65,6 +71,80 @@ local function moveControlPoints(curve, n, dx, dy)
 	end
 end
 
+local function moveOtherControlPoint(curve, n, dx, dy)
+	local e = endpointIndex(n)
+	local p, ep = curve[n], curve[e]
+	local constraint = ep.constraint
+	local o = curve[otherControlPoint(curve, n)]
+	if constraint == 'smooth' then
+		local ax, ay = p[1] - ep[1], p[2] - ep[2]
+		local bx, by = o[1] - ep[1], o[2] - ep[2]
+		local a2, b2 = ax*ax + ay*ay, bx*bx + by*by
+		if a2 > 0.001 and b2 > 0.001 then
+			local scale = math.sqrt(b2 / a2)
+			o[1] = ep[1] - ax * scale
+			o[2] = ep[2] - ay * scale
+		end
+	elseif constraint == 'symmetric' then
+		o[1] = 2*ep[1] - p[1]
+		o[2] = 2*ep[2] - p[2]
+	end
+end
+
+local function setEndpointConstraint(curve, n, constraint)
+	local e = endpointIndex(n)
+	local p, ep, q = curve[e-1], curve[e], curve[e+1]
+	-- Save old values
+	local ret = { curve, e, ep.constraint, {unpack(p)}, {unpack(q)} }
+
+	-- Offset of each point.
+	local px, py = p[1] - ep[1], p[2] - ep[2]
+	local qx, qy = q[1] - ep[1], q[2] - ep[2]
+	-- Flip them in the same direction and average them.
+	local dx, dy = 0.5 * (px - qx), 0.5 * (py - qy)
+	-- Lengths of the above.
+	local p2 = px*px + py*py
+	local q2 = qx*qx + qy*qy
+	local d2 = dx*dx + dy*dy
+	if d2 < 0.001 then
+		if p2 < 0.001 then
+			if q2 < 0.001 then return  -- everything is zero.
+			else dx, dy, d2 = qx, qy, q2 end
+		else dx, dy, d2 = px, py, p2 end
+	end
+
+	ep.constraint = constraint
+	if constraint == 'smooth' then
+		local ps = math.sqrt(p2 / d2)
+		local qs = math.sqrt(q2 / d2)
+		p[1], p[2] = ep[1] + dx * ps, ep[2] + dy * ps
+		q[1], q[2] = ep[1] - dx * qs, ep[2] - dy * qs
+	elseif constraint == 'symmetric' then
+		p[1], p[2] = ep[1] + dx, ep[2] + dy
+		q[1], q[2] = ep[1] - dx, ep[2] - dy
+	end
+
+	return unpack(ret)
+end
+
+local function undoConstraint(curve, e, c, cp1, cp2)
+	local p, ep, q = curve[e-1], curve[e], curve[e+1]
+	p[1], p[2] = unpack(cp1)
+	ep.constraint = c
+	q[1], q[2] = unpack(cp1, cp2)
+end
+
+local function toggleConstraint(curve, n)
+	local e = endpointIndex(n)
+	if n ~= e or n == 1 or n == #curve then return end
+	local ep = curve[e]
+	local constraint
+	if ep.constraint == 'smooth' then constraint = 'symmetric'
+	elseif ep.constraint == 'symmetric' then constraint = nil
+	else constraint = 'smooth' end
+	edits:perform('setEndpointConstraint', curve, n, constraint)
+end
+
 local function moveBezierPoint(curve, n, x, y)
 	local p = curve[n]
 	local undoX, undoY = p[1], p[2]
@@ -72,6 +152,11 @@ local function moveBezierPoint(curve, n, x, y)
 	if isEndpoint(n) then
 		local dx, dy = x - undoX, y - undoY
 		moveControlPoints(curve, n, dx, dy)
+	else
+		local e = endpointIndex(n)
+		if curve[e].constraint and otherControlPoint(curve, n) then
+			moveOtherControlPoint(curve, n, dx, dy)
+		end
 	end
 	return curve, n, undoX, undoY
 end
@@ -117,6 +202,7 @@ function love.load()
 	edits:command('extendBezier', extendBezier, retractBezier)
 	edits:command('moveBezierPoint', moveBezierPoint, moveBezierPoint, moveBezierPoint)
 	edits:command('deleteBezierPoint', deleteBezierPoint, insertBezierPoint)
+	edits:command('setEndpointConstraint', setEndpointConstraint, undoConstraint)
 
 	love.graphics.setLineWidth(3)
 end
@@ -206,6 +292,10 @@ function love.keypressed(k, s)
 		elseif k == 'delete' or k == 'backspace' then
 			if curve.highlight then
 				edits:perform('deleteBezierPoint', curve, curve.highlight)
+			end
+		elseif k == 'c' then
+			if curve.highlight then
+				toggleConstraint(curve, curve.highlight)
 			end
 		end
 	end
