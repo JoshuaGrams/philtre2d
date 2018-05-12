00398be1ad9b1a67533918974aefe2037521897e
Josh Grams <josh@qualdan.com>
2018-05-12 07:58:59 -0400

    Add widget functions to Sprite (request/allocate).

--- engine/Sprite.lua
+++ engine/Sprite.lua
@@ -15,6 +15,23 @@ function Sprite.draw(self)
 	love.graphics.draw(self.image, -self.ox, -self.oy)
 end
 
+function Sprite.request(self)
+	return self._req
+end
+
+function Sprite.allocate(self, x, y, w, h)
+	local c, s = math.cos(self.angle), math.sin(self.angle)
+	local ac, as = math.abs(c), math.abs(s)
+	local iw, ih = self.image:getDimensions()
+	local rw, rh = iw * ac + h * as, iw * as + h * ac
+	local scale = math.min(w / rw, h / rh)
+	self.sx, self.sy = scale, scale
+	local ox = (self.ox - iw/2) * scale
+	local oy = (self.oy - ih/2) * scale
+	self.pos.x = x + w/2 + ox * c - oy * s
+	self.pos.y = y + h/2 + ox * s + oy * c
+end
+
 function Sprite.set(self, image, x, y, angle, sx, sy, color, ox, oy, kx, ky)
 	assert(image, 'Sprite() - must specify a texture file path or Image object.')
 	Sprite.super.set(self, x, y, angle, sx, sy, kx, ky)
@@ -27,6 +44,7 @@ function Sprite.set(self, image, x, y, angle, sx, sy, color, ox, oy, kx, ky)
 	self.image = image
 	self.color = color or {255, 255, 255, 255}
 	local w, h = image:getDimensions()
+	self._req = { w = w, h = h }
 	ox = ox or 'center';  oy = oy or 'center'
 	if type(ox) == 'string' then  ox = w * origins[ox]  end
 	if type(oy) == 'string' then  oy = h * origins[oy]  end
