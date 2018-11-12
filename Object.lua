364dedd17926d5120da02ac037718bd76a51f042
rgrams <ross.grams@gmail.com>
2018-11-12 13:32:42 -0500

    Object - Draw with circle(), points() breaks batching. - Also switch to v11 color (not that it matters for white).

--- Object.lua
+++ Object.lua
@@ -64,9 +64,9 @@ function Object.setVisible(self, visible)
 end
 
 function Object.draw(self)
-	love.graphics.setColor(255, 255, 255)
+	love.graphics.setColor(1, 1, 1)
 	love.graphics.rectangle('line', -5, -5, 10, 10)
-	love.graphics.points(0, 0)
+	love.graphics.circle('line', 0, 0, 0.5, 4)
 end
 
 function Object.set(self, x, y, angle, sx, sy, kx, ky)
