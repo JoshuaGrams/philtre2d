e184a835578116c6d794630912d2a9f83a8f1e73
Josh Grams <josh@qualdan.com>
2019-04-13 14:23:48 -0400

    Text: add vertical alignment.

--- Text.lua
+++ Text.lua
@@ -3,26 +3,31 @@ local Object = require(base .. 'Object')
 local Text = Object:extend()
 Text.className = 'Text'
 
+local validAlignment = { center = true, left = true, right = true, justify = true }
+local hOffset = { center = -0.5, left = 0, right = -1, justify = 0 }
+local vOffset = { top = 0, middle = -0.5, bottom = -1 }
+
 function Text.draw(s)
 	love.graphics.setBlendMode(s.blendMode)
 	love.graphics.setFont(s.font)
 	love.graphics.setColor(s.color)
 	if s.wrapLimit then
-		love.graphics.printf(s.text, 0, 0, s.wrapLimit, s.align, s.angle, 1, 1, 0, 0, s.kx, s.ky)
+		local ox = hOffset[s.hAlign] * s.wrapLimit
+		local oy = vOffset[s.vAlign] * s.font:getHeight()
+		love.graphics.printf(s.text, 0, 0, s.wrapLimit, s.hAlign, s.angle, 1, 1, 0, 0, s.kx, s.ky)
 	else
 		love.graphics.print(s.text, 0, 0, s.angle, 1, 1, 0, 0, s.kx, s.ky)
 	end
 end
 
-local validAlignment = { center = true, left = true, right = true, justify = true }
-
-function Text.set(self, text, font, x, y, angle, wrapLimit, align, sx, sy, kx, ky)
+function Text.set(self, text, font, x, y, angle, wrapLimit, hAlign, sx, sy, kx, ky)
 	Text.super.set(self, x, y, angle, sx, sy, kx, ky)
 	self.blendMode = 'alpha'
 	self.color = {1, 1, 1, 1}
 	self.text = text
 	self.wrapLimit = wrapLimit
-	self.align = validAlignment[align] and align or 'left'
+	self.hAlign = validAlignment[hAlign] and hAlign or 'left'
+	self.vAlign = (self.hAlign == 'center') and 'middle' or 'top'
 	if type(font) == 'table' then -- {filename, size}
 		self.font = new.font(unpack(font))
 	else
