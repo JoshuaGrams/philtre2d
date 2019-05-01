c48e07d02c0bd177555e146b651f35891e921eb8
rgrams <ross.grams@gmail.com>
2019-05-01 07:59:22 -0400

    Layout.Box: Pass allocation on to children.

--- layout/Box.lua
+++ layout/Box.lua
@@ -16,6 +16,11 @@ function Box.request(b)  return b._req  end
 
 function Box.allocate(b, x, y, w, h)
 	b.pos.x, b.pos.y, b.width, b.height = x, y, w, h
+	if b.children then
+		for i,v in ipairs(b.children) do
+			if v.allocate then  v:allocate(x, y, w, h)  end
+		end
+	end
 end
 
 return Box
