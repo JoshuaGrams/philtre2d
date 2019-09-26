5c67b85fc53146d2d7eceed878c391944d70774b
rgrams <ross.grams@gmail.com>
2019-09-26 15:32:34 -0400

    World: Add World:draw to draw contacts.

--- World.lua
+++ World.lua
@@ -6,6 +6,56 @@ local World = Object:extend()
 
 World.className = 'World'
 
+local COLOR_NORMAL_ENABLED = {0, 1, 1, 1}
+local COLOR_NORMAL_DISABLED = {1, 0, 0, 1}
+local COLOR_CONTACT_PT_1 = {1, 0, 0, 1}
+local COLOR_CONTACT_PT_2 = {1, 0.5, 0, 1}
+local SIZE_CONTACT_PT_1 = 5
+local SIZE_CONTACT_PT_2 = 3
+local COLOR_CONTACT_PTS_LINE = {1, 0, 1, 0.5}
+local NORMAL_LENGTH = 25
+local NORMAL_ARROW_LENGTH = 10
+local NORMAL_ARROW_ANGLE = 0.5
+
+local function vec2_rotate(ax, ay, phi)
+	local c = math.cos(phi);  local s = math.sin(phi)
+	return c * ax - s * ay, s * ax + c * ay
+end
+
+function World.draw(self) -- Debug drawing for contacts.
+	for i,contact in ipairs(self.world:getContacts()) do
+		local x1, y1, x2, y2 = contact:getPositions()
+		local isEnabled = contact:isEnabled()
+		if x1 then
+			-- Draw normal line.
+			local col = isEnabled and COLOR_NORMAL_ENABLED or COLOR_NORMAL_DISABLED
+			love.graphics.setColor(col)
+			local nx, ny = contact:getNormal()
+			local l1, l2 = NORMAL_LENGTH, NORMAL_ARROW_LENGTH
+			local endx, endy = x1 + nx*l1, y1 + ny*l1
+			love.graphics.line(x1, y1, endx, endy)
+			-- Draw normal arrow edges.
+			local v1x, v1y = vec2_rotate(nx, ny, NORMAL_ARROW_ANGLE)
+			local v2x, v2y = vec2_rotate(nx, ny, -NORMAL_ARROW_ANGLE)
+			love.graphics.line(endx, endy, endx - v1x*l2, endy - v1y*l2)
+			love.graphics.line(endx, endy, endx - v2x*l2, endy - v2y*l2)
+			-- Draw contact point.
+			love.graphics.setColor(COLOR_CONTACT_PT_1)
+			love.graphics.setPointSize(SIZE_CONTACT_PT_1)
+			love.graphics.points(x1, y1)
+		end
+		if x2 then -- Draw second contact point.
+			love.graphics.setColor(COLOR_CONTACT_PT_2)
+			love.graphics.setPointSize(SIZE_CONTACT_PT_2)
+			love.graphics.points(x2, y2)
+			if x1 then -- Both contact points exist, draw line between them.
+				love.graphics.setColor(COLOR_CONTACT_PTS_LINE)
+				love.graphics.line(x1, y1, x2, y2)
+			end
+		end
+	end
+end
+
 local fix = {}
 local obj = {}
 
