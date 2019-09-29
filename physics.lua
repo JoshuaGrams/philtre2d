6987097d3ca8000c642340a916e3839aba120d26
rgrams <ross.grams@gmail.com>
2019-09-28 20:16:20 -0400

    Physics: Add raycast with collision filtering.

--- physics.lua
+++ physics.lua
@@ -46,15 +46,12 @@ end
 
 -- Takes two "category" and "mask" bitmask pairs.
 -- Returns if they collide (true/false).
-local function shouldCollide(catBitsA, maskBitsA, catBitsB, maskBitsB)
-	return (bit.band(maskBitsA, catBitsB) ~= 0) and (bit.band(catBitsA, maskBitsB) ~= 0)
+local function shouldCollide(catsA, maskA, catsB, maskB)
+	return (bit.band(maskA, catsB) ~= 0) and (bit.band(catsA, maskB) ~= 0)
 end
 
--- ray cast
-	-- multi-hit - sorted near-to-far
-	-- only-closest?
-
 local queryResults = {}
+local queryCats, queryMask
 
 local function boxQueryCallback(fixture)
 	table.insert(queryResults, fixture)
@@ -83,6 +80,28 @@ local function atPoint(x, y, world)
 	end
 end
 
+local function raycastCallback(fixture, x, y, xn, yn, fraction)
+	if queryCats and queryMask then
+		local cats, mask = fixture:getFilterData()
+		local canHit = shouldCollide(cats, mask, queryCats, queryMask)
+		if not canHit then  return 1  end
+	end
+	local hit = {
+		fixture = fixture,
+		x = x, y = y, xn = xn, yn = yn, fraction = fraction
+	}
+	table.insert(queryResults, hit)
+	return 1
+end
+
+local function raycast(x1, y1, x2, y2, world, categories, mask)
+	for i, v in ipairs(queryResults) do  queryResults[i] = nil  end
+	queryCats, queryMask = categories, mask
+	world:rayCast(x1, y1, x2, y2, raycastCallback)
+	if #queryResults > 0 then
+		return queryResults
+	end
+end
 local function getWorld(self)
 	local parent = self.parent
 	if not parent then
@@ -99,5 +118,6 @@ return {
 	getCategoriesBitmask = getCategoriesBitmask,
 	getMaskBitmask = getMaskBitmask,
 	isInCategory = isInCategory, shouldCollide = shouldCollide,
-	atPoint = atPoint, touchingBox = touchingBox, getWorld = getWorld,
+	atPoint = atPoint, touchingBox = touchingBox, raycast = raycast,
+	getWorld = getWorld,
 }
