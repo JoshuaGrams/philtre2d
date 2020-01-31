4508b3e575c32dbd9e5c4790d643099eccf9159f
rossgrams <ross.grams@gmail.com>
2020-01-31 11:17:00 -0500

    Layer: Add mask handling. - Also renamed a couple vars.

--- layer.lua
+++ layer.lua
@@ -21,25 +21,51 @@ function Layer.addObject(self, object)
 	addFunction(self, m, object.call, object, 'draw')
 end
 
+-- Reset to origin and apply object's to-world transform.
+local function applyMaskObjectTransform(obj)
+	love.graphics.push()
+	love.graphics.origin()
+	local m = obj._to_world
+	local th, sx, sy, kx, ky = M.parameters(m)
+	love.graphics.translate(m.x, m.y)
+	love.graphics.rotate(th)
+	love.graphics.scale(sx, sy)
+	love.graphics.shear(kx, ky)
+end
+
 function Layer.draw(self)
-	local m = nil
+	local curMatrix = nil
+	local curMaskObj = nil
 	for i=1,#self do
 		if i > self.n then
 			self[i] = nil
 		else
-			local fn = self[i]
+			local params = self[i]
 			local pushed
-			if m ~= fn.m then
-				m, pushed = fn.m, true
-				local th, sx, sy, kx, ky = M.parameters(m)
+			if curMatrix ~= params.m then
+				curMatrix, pushed = params.m, true
+				local th, sx, sy, kx, ky = M.parameters(curMatrix)
 				love.graphics.push()
-				love.graphics.translate(m.x, m.y)
+				love.graphics.translate(curMatrix.x, curMatrix.y)
 				love.graphics.rotate(th)
 				love.graphics.scale(sx, sy)
 				love.graphics.shear(kx, ky)
 			end
-			fn[1](unpack(fn, 2))
-			if pushed then love.graphics.pop() end
+			local maskObj = params[2].maskObject
+			if curMaskObj ~= maskObj then
+				if maskObj then
+					applyMaskObjectTransform(maskObj)
+					maskObj:enableMask()
+					love.graphics.pop()
+				elseif curMaskObj then
+					applyMaskObjectTransform(curMaskObj)
+					curMaskObj:disableMask()
+					love.graphics.pop()
+				end
+				curMaskObj = maskObj
+			end
+			params[1](unpack(params, 2))
+			if pushed then  love.graphics.pop()  end
 		end
 	end
 end
