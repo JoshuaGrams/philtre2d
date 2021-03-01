a3d4f89a295a89b56b51ff54bcdeef3f8d45fc4a
rgrams <ross.grams@gmail.com>
2021-03-01 13:02:37 -0500

    Gui.Node: Fix Y value of "SE" cardinal direction.

--- objects/gui/Node.lua
+++ objects/gui/Node.lua
@@ -10,7 +10,7 @@ local max = math.max
 local sin, cos = math.sin, math.cos
 
 local CARDINALS = {
-	N = {0, -1}, NE = {1, -1}, E = {1, 0}, SE = {1, -1},
+	N = {0, -1}, NE = {1, -1}, E = {1, 0}, SE = {1, 1},
 	S = {0, 1}, SW = {-1, 1}, W = {-1, 0}, NW = {-1, -1}, C = {0, 0}
 }
 for k,v in pairs(CARDINALS) do  CARDINALS[string.lower(k)] = v  end
