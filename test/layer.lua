a9f4d18b40a523b2a35792ceede66fbc8d624fcc
Ross Grams <ross.grams@gmail.com>
2021-11-16 19:46:57 -0500

    Layer Tests: Change to test behavior instead of implementation (mostly)

--- test/layer.lua
+++ test/layer.lua
@@ -5,106 +5,128 @@ local Layer = require(base .. 'render.layer')
 local M = require(base .. 'modules.matrix')
 local Object = require(base .. 'objects.Object')
 
-local function recordCall(called, name)
+local function identityM()
+	return M.copy(M.identity)
+end
+
+local function recorderFn(called, name)
 	table.insert(called, name)
 end
 
-local function mod(obj, props)
-	for name,prop in pairs(props) do
-		obj[name] = prop
-	end
+local _called
+local function recorderDraw(self)
+	table.insert(_called, self.name)
+end
+
+local function makeNumberedObject(i)
+	local obj = Object()
+	obj.name = i
+	obj.draw = recorderDraw
 	return obj
+end
+
+local function makeObjects(i1, i2)
+	local list = {}
+	for i=i1,i2 do
+		table.insert(list, makeNumberedObject(i))
+	end
+	return list
+end
+
+local function addObjects(layer, objects)
+	for i,obj in ipairs(objects) do  layer:addObject(obj)  end
+	return objects
 end
 
 return {
 	"Layers",
-	setup = function()
-		return M.identity
-	end,
-	function(m)
-		local called = {}
+	function()
+		_called = {}
 		local layer = Layer()
 		local names = { 'first', 'second', 'third', 'fourth' }
 		for _,name in ipairs(names) do
-			layer:addFunction(m, recordCall, called, name)
+			layer:addFunction(identityM(), recorderFn, _called, name)
 		end
 		layer:draw()
-		T.is(#called, #names, "called correct number of functions")
-		T.has(called, names, "should call functions in order")
+		T.is(#_called, #names, "called correct number of functions")
+		T.has(_called, names, "called functions in order") -- T.has checks matching key-value pairs.
 
-		called = {}
+		_called = {}
 		layer:clear()
-		layer:addFunction(m, recordCall, called, 'fifth')
-		layer:addFunction(m, recordCall, called, 'sixth')
 		layer:draw()
-		T.is(#called, 2, "can have fewer functions after being cleared")
-		T.has(called, {
+		T.is(next(_called), nil, "clear removed everything")
+
+		_called = {}
+		layer:addFunction(identityM(), recorderFn, _called, 'fifth')
+		layer:addFunction(identityM(), recorderFn, _called, 'sixth')
+		layer:draw()
+		T.is(#_called, 2, "can have fewer functions after being cleared")
+		T.has(_called, {
 			'fifth', 'sixth'
 		}, "should call only new functions after being cleared")
 
-		called = {}
+		_called = {}
 		layer:clear()
-		layer:addFunction(m, recordCall, called, 'seventh')
-		layer:addFunction(m, recordCall, called, 'eighth')
-		layer:addFunction(m, recordCall, called, 'ninth')
-		layer:addFunction(m, recordCall, called, 'tenth')
-		layer:addFunction(m, recordCall, called, 'eleventh')
-		layer:addFunction(m, recordCall, called, 'twelfth')
+		layer:addFunction(identityM(), recorderFn, _called, 'seventh')
+		layer:addFunction(identityM(), recorderFn, _called, 'eighth')
+		layer:addFunction(identityM(), recorderFn, _called, 'ninth')
+		layer:addFunction(identityM(), recorderFn, _called, 'tenth')
+		layer:addFunction(identityM(), recorderFn, _called, 'eleventh')
+		layer:addFunction(identityM(), recorderFn, _called, 'twelfth')
 		layer:draw()
-		T.is(#called, 6, "can have more functions after being cleared")
-		T.has(called, {
+		T.is(#_called, 6, "can have more functions after being cleared")
+		T.has(_called, {
 			'seventh', 'eighth', 'ninth',
 			'tenth', 'eleventh', 'twelfth'
 		}, "should call only new functions after being cleared")
 	end,
 	function()
+		-- Make sure object has a numeric drawIndex and gets drawn.
 		local layer = Layer()
-		local obj = mod(Object(), {name = 1})
+		local obj = makeObjects(1, 1)[1]
 		layer:addObject(obj)
-		T.ok(layer[1] and layer[1][2] == obj, "added object")
-		T.ok(layer.n == 1, "  layer.n == 1")
-		T.ok(obj.drawIndex == 1, "  object's drawIndex == 1")
+		T.ok(tonumber(obj.drawIndex), "Added object has numeric drawIndex")
 
-		local obj2 = mod(Object(), {name = 2})
-		local obj3 = mod(Object(), {name = 3})
-		local obj4 = mod(Object(), {name = 4})
-		local obj5 = mod(Object(), {name = 5})
-		layer:addObject(obj2)
-		layer:addObject(obj3)
-		layer:addObject(obj4)
-		T.ok(layer[2][2] == obj2 and layer[3][2] == obj3 and layer[4][2] == obj4,"added 3 more objects")
-		T.ok(layer.n == 4, "  layer.n == 4")
-		T.ok(obj2.drawIndex == 2 and obj3.drawIndex == 3 and obj4.drawIndex == 4, "  objects' drawIndices are 2, 3, and 4")
+		_called = {}
+		layer:draw()
+		T.has(_called, {1}, "Added object was drawn")
 
-		layer:removeObject(obj2)
-		T.ok(layer[1] and layer[3] and layer[4] and (layer[2] == false), "removed object, its index is now false, all others still exist")
-		T.ok(layer.dirty == true, "  layer is now flagged as dirty")
-		T.ok(layer.n == 4, "  layer.n is still 4")
+		-- Make sure consecutively added objects have consecutive drowIndices and are drawn in order.
+		local objs = addObjects(layer, makeObjects(2, 5))
+		table.insert(objs, 1, obj)
+		_called = {}
+		local inOrder =
+			objs[1].drawIndex < objs[2].drawIndex and
+			objs[2].drawIndex < objs[3].drawIndex and
+			objs[3].drawIndex < objs[4].drawIndex and
+			objs[4].drawIndex < objs[5].drawIndex
+		T.ok(inOrder, "Added more objects and their drawIndices are in ascending order")
 
-		layer:addObject(obj5)
-		T.ok(layer[5][2] == obj5 and layer.n == 5, "added a 5th object, it is at index 5 and layer.n is now 5")
-		T.ok(obj5.drawIndex == 5, "  5th object's drawIndex is 5")
+		layer:draw()
+		T.has(_called, {1,2,3,4,5}, "Added more objects and they are drawn in order")
 
-		layer:refreshIndices()
-		T.note("layer:refreshIndices()")
-		T.ok(layer[2] ~= false, "layer's second element is no longer false")
-		T.ok(layer[2][2] == obj3, "  it is now object 3")
-		T.ok(layer.n == 4, "refreshed indices, layer.n is now 4")
-		T.ok(
-			obj.drawIndex == 1 and obj3.drawIndex == 2 and obj4.drawIndex == 3 and obj5.drawIndex == 4,
-			"objects' drawIndices have been updated and to be consecutive with no gaps"
-		)
+		-- Make sure objects still get drawn in order after second object is removed.
+		layer:removeObject(objs[2])
+		T.ok(not objs[2].drawIndex, "Remove object's drawIndex has been made falsy")
+		_called = {}
+		layer:draw()
+		T.has(_called, {1,3,4,5}, "Removed 2nd object and others are still drawn in order")
 
-		layer:removeObject(obj3)
-		layer:removeObject(obj4)
-		T.ok(layer[2] == false and layer[3] == false and layer[1] and layer[4], "removed two objects, their indices are now false, others still exist")
-		T.ok(layer.n == 4, "layer.n is still 4")
+		-- Make sure a new object has a higher drawIndex than the others, and all get drawn.
+		table.insert(objs, makeNumberedObject(6))
+		layer:addObject(objs[6])
+		T.ok(objs[6].drawIndex > objs[5].drawIndex, "Added new object and its index is higher than the last")
 
-		layer:refreshIndices()
-		T.note("layer:refreshIndices() -- two consecutive removed objects")
-		T.ok(layer[2] ~= false and layer[3] ~= false, "refresh indices, layer[2] and layer[3] are no longer false")
-		T.ok(layer[2][2] == obj5 and layer[3] == nil and layer[4] == nil, "second item is now object-5, 3rd and 4th items are now nil.")
-		T.ok(layer.n == 2, "layer.n is now 2")
-		T.ok(obj.drawIndex == 1 and obj5.drawIndex == 2, "objects' drawIndices are updated correctly")
+		_called = {}
+		layer:draw()
+		T.ok(_called[5] == 6, "New object is drawn in the correct order")
+
+		-- Remove a couple consecutive objects and re-check draw order.
+		layer:removeObject(objs[3])
+		layer:removeObject(objs[4])
+		T.ok(not objs[3].drawIndex and not objs[4].drawIndex, "Removed two more objects, their drawIndices were cleared")
+		_called = {}
+		layer:draw()
+		T.has(_called, {1,5,6}, "Removed two consecutive objects from the middle, the remaining objects still drawn in order")
 	end
 }
